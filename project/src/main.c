61d8e83aea07fc133f60ae51146e05cffcb5a092
Lucy H. <velossel@gmail.com>
2025-11-16 22:56:58 -0700

    Transferred main

--- project/src/main.c
+++ project/src/main.c
@@ -1,1 +1,12 @@
-#main 
+//Initialize program
+#include "toy.h"
+
+int main(void)
+{
+    init_hardware();
+
+    while (1) {
+        update_state();
+        __low_power_mode_0(); // save power while waiting for button
+    }
+}

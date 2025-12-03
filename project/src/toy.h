1ea6a0d17f793a540d10fb75690a32954848a256
Lucy H. <velossel@gmail.com>
2025-12-03 15:58:49 -0700

    Refactor toy.h for clarity and fix type typo
    Updated comments for clarity and fixed a typo in the state variable type.

--- project/src/toy.h
+++ project/src/toy.h
@@ -4,37 +4,36 @@
 #include <msp430.h>
 #include <stdint.h>
 
-//Define our constants in out LEDs and pins
+// LED & button pin definitions
+#define LED_RED     BIT0   // P1.0
+#define LED_GREEN   BIT6   // P1.6
 
-#define LED_RED BIT0 //P1.0
-#define LED_GREEN BIT6 //P1.6
-#define BTN0 BIT0 //P2.0
-#define BTN1 BIT1 //P2.1
-#define BTN2 BIT2 //P2.2
-#define BTN3 BIT3 //P2.3
+#define BTN0 BIT0   // P2.0
+#define BTN1 BIT1   // P2.1
+#define BTN2 BIT2   // P2.2
+#define BTN3 BIT3   // P2.3
 
 #define NUM_STATES 5
 
-//Frequency values for tone (In Hz)
+// Tone frequencies (Hz)
 #define NOTE_C4 261
 #define NOTE_E4 329
 #define NOTE_G4 392
 #define NOTE_C5 523
 
-//State machine stuff below
-
-typedef enum{
-  IDLE = 0,
-  PLAY_TUNE,
-  FLASH_FAST,
-  FLASH_SLOW,
-  FREERUN
+// State machine states
+typedef enum {
+    IDLE = 0,
+    PLAY_TUNE,
+    FLASH_FAST,
+    FLASH_SLOW,
+    FREERUN
 } toy_state_t;
 
-extern volatile uint8t_t state; //states
+// ❗ FIXED TYPO: should be uint8_t, not uint8t_t
+extern volatile uint8_t state;
 
-//Functions
-
+// Functions implemented in C
 void init_hardware(void);
 void update_state(void);
 void play_melody(void);
@@ -43,9 +42,9 @@ void set_led_pattern_fast(void);
 void do_idle(void);
 void do_random_effects(void);
 
-//assembly function
-
-void asm_next_state(void);
+// Function implemented in assembly
+// Assembly file MUST contain:   .global asm_next_state
+// and a RET
+extern void asm_next_state(void);
 
 #endif
-
